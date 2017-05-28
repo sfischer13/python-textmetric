1e8a4cd7be4e25bf42ea91e34904d41eccba6b1c
Stefan Fischer <sfischer13@ymail.com>
2017-05-28 15:58:40 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+]
+
+test_requirements = [
+]
+
+setup(
+    name='textmetric',
+    version='0.0.2',
+    description='Python implementations of common text metric algorithms.',
+    long_description=readme + '\n\n' + history,
+    author='Stefan Fischer',
+    author_email='sfischer13@ymail.com',
+    url='https://github.com/sfischer13/python-textmetric',
+    packages=[
+        'textmetric',
+    ],
+    package_dir={'textmetric': 'textmetric'},
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT',
+    zip_safe=False,
+    keywords='text metric density readability TTR',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
